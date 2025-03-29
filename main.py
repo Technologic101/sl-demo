dcbcdae84d336a3f2fc6ec4690a117598d4263b3
Anthony Chapman <adc@thewebdev.us>
2025-03-28 21:22:24 -0400

    Day 5: st.write demo

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import altair as alt
 
-def main():
-    st.header("st.button")
-    if st.button("Click me!"):
-        st.write("Why hello there")
-    else:
-        st.write("Goodbye")
+st.header("Write Demo")
 
+# Write can contain styles and markdown
+st.write("Hello *World* :sunglasses:")
 
-if __name__ == "__main__":
-    main()
+# Write can also contain numbers
+st.write(1234)
+
+# Write can also contain dataframes
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Write displays elements in a column
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# Write can also display altair charts
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
